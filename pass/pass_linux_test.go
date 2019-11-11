8f8d153d8068d25873a002ab925822e47fea9d79
Jakub <jakub@cuth.sk>
2019-11-12 00:22:37 +0100

    [t] desing the encode / decode of long records

--- pass/pass_linux_test.go
+++ pass/pass_linux_test.go
@@ -73,3 +73,78 @@ func TestPassHelper(t *testing.T) {
 		t.Fatal("didn't delete all creds?")
 	}
 }
+
+const maxFileNameLength = 120 // encrypted ext4 max 143, keep it bellow 128 and have reserve for suffix
+
+func encodePath(serverURL, username) string {
+	// separate serverURL and username
+	serverAndUsername = strings.Join(
+		[]string{strings.TrimSuffix(serverURL, username), username}, "\x00",
+	)
+	// encode base64
+	encoded := base64.URLEncoding.EncodeToString([]byte(serverAndUsername))
+	// split if folder name too long
+	splited := []string{}
+	part := ""
+	for i, r := range encoded {
+		if i%maxFileNameLength == 0 && i != 0 && len(encoded) {
+			splited = append(splited, part)
+			part = ""
+		}
+		part += r
+	}
+	if len(part) != 0 {
+		splited = append(splited, part)
+	}
+	// add data file
+	splited = append(splited, "data.gpg")
+	return filepath.Join(splited)
+}
+
+func decodePath(path string) (serverURL, username string, err error) {
+	// split folders and check encoded path
+	splited := filepath.Split(path)
+	if len(splited) < 2 || splited[len(splited)] != "data.gpg" {
+		return "", "", errors.New("incorrect path encoding: no data.gpg")
+	}
+	// join and decode
+	decoded, err := base64.URLEncoding.DecodeString(
+		strings.Join(splited[:len(splited)-1], ""),
+	)
+	if err != nil {
+		return "", "", errors.Wrap(err, "incorrect path encoding")
+	}
+	// separate and check serverURL and username
+	serverAndUsername := strings.Split(decoded, "\x00")
+	if len(serverAndUsername) != 2 {
+		return "", "", errors.New("incorrect path encoding: no delimiter")
+	}
+
+	username = serverAndUsername[1]
+	serverURL = serverAndUsername[0] + username
+
+	return
+}
+
+func TestEncodeDecodePath(t *testing.T) {
+	username := "jMzPG9_Vi8jxr6GqkLm05igLcX04kR5Jq15A_safAM49vTqtoWn8CLhiQR9uOwHxpnwLDtCDGYIaq8Y2QfiMeQ=="
+	serverURL := "protonmail/bridge/users/" + username
+	expPath := "cHJvdG9ubWFpbC9icmlkZ2UvdXNlcnMvAGpNelBHOV9WaThqeHI2R3FrTG0wNWlnTGNYMDRrUjVKcTE1QV9zYWZBTTQ5dlRxdG9XbjhDTGhpUVI5dU93SHhw/bndMRHRDREdZSWFxOFkyUWZpTWVRPT0=/data.gpg"
+
+	// separate server and username
+	path := encodePath(serverURL, username)
+	if expPath != path {
+		t.Failf(" expected path %q but got %q", expPath, path)
+	}
+
+	decURL, decUsername, err = decodePath(expPath)
+	if err != nil {
+		t.Failf("error while decodePath: %v", err)
+	}
+	if decURL != serverURL {
+		t.Failf(" expected URL %q but got %q", serverURL, decURL)
+	}
+	if decUsername != username {
+		t.Failf(" expected username %q but got %q", decUsername, username)
+	}
+}
